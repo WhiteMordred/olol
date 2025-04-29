0e5e4395bb04335edf408f2a66098e9873ccad0f
White Mordred <whitemordred@dev2real.com>
2025-04-29 14:36:07 +0200

    Update

--- src/olol/proxy/__init__.py
+++ src/olol/proxy/__init__.py
@@ -1,7 +1,7 @@
 """Package proxy pour le load balancing d'Ollama"""
 
 from .app import run_proxy, app
-from .console_ui import ConsoleUI, run_console_ui
+from .console_ui import RichUI, run_console_ui
 from .health import health_checker
 from .stats import update_request_stats, request_stats, stats_lock
 from .utils import create_grpc_client, adjust_context_length
